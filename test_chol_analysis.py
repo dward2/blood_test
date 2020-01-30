82df83cd8bb62801c640a75c3e7830d2830e5f22
David Ward <david.a.ward@duke.edu>
2020-01-30 13:22:43 -0500

    added second test function

--- test_chol_analysis.py
+++ test_chol_analysis.py
@@ -2,5 +2,12 @@ def test_HDL_analysis():
     from chol_analysis import HDL_analysis
     answer = HDL_analysis(100)
     expected = 'Normal'
-    assert answer = expected
+    assert answer == expected
+    
+
+def test_HDL_analysis_2():
+    from chol_analysis import HDL_analysis
+    answer = HDL_analysis(40)
+    expected = 'Borderline low'
+    assert answer == expected
    
